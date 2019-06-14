572eaf4d6b7e4e8fcfb3c4dc5ac22d2196131a2a
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-06-14 12:13:59 +1000

    weather api added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
     # filters execute a bunch of code before the intended method executes and after the intended method executes
     before_action :before_data
     after_action :after_data
+    # before_action :consume_api
 
     def home
         # @params_info = params[:profile]
@@ -11,7 +12,7 @@ class PagesController < ApplicationController
         # render "shared/nav"
         # params is a variable that stores the data coming from the routes, which could be used in our controller
         puts "params info-------"
-        @params_info = params[:profile]
+        # @params_info = params[:profile]
         puts "params info ------"
         # instead of sending a html file, lets send a plain text
         # render fetches the data in the form of a plain text, json or a html
@@ -25,17 +26,29 @@ class PagesController < ApplicationController
  
 
         # redirect_to redirects to another method/page
-        redirect_to root_path
+        # redirect_to root_path
 
         # redirect_to "/pages/contact"
+        # call the consume api method
+        consume_api
+        # @profile = {name: :nands, age: 10, bio: :hello}
+        render json: @data
+
     end
 
     def before_data
         puts "method executed before the data method"
-        @params_info = parms[:profile]
+        # @params_info = params[:profile]
     end
 
     def after_data
         puts "method executed after the data method"
+    end
+
+    def consume_api
+        url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+        response =  RestClient.get(url)
+        @data = JSON.parse(response.body)["weather"]
+        puts @data
     end
 end
